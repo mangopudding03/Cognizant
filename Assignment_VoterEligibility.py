b1b5d41da9f9c3442c18a78380cb4ab8b43600c0
mangopudding03 <100097596+mangopudding03@users.noreply.github.com>
2025-05-14 21:03:14 -0400

    commit2

--- Assignment_VoterEligibility.py
+++ Assignment_VoterEligibility.py
@@ -7,5 +7,5 @@ if age >= 18: #Legal age to vote
 else:
     years_left = 18 - age
     #Letting them know they're ineligible and the amount of years they still have left to be able to vote
-    print(f"Oops! You're not eligible yet. But hey, only {years_left} more year{'s' if years_left > 1 else ''} to go!")
+    print(f"Oops! You're not eligible yet. But hey, only {years_left} more year{'s' if years_left > 1 else ''} to gooooo!")
 
