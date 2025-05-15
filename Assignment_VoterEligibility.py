a0e6312ded865cf77e137956942935cf0fdcc056
mangopudding03 <100097596+mangopudding03@users.noreply.github.com>
2025-05-14 20:50:20 -0400

    Voter Eligibility

---
+++ Assignment_VoterEligibility.py
@@ -0,0 +1,11 @@
+# Voter Eligibility
+
+age = int(input("How old are you? ")) #getting the user's age
+
+if age >= 18: #Legal age to vote
+    print("Congratulations! You are eligible to vote. Go make a difference!") #Letting them know they can vote
+else:
+    years_left = 18 - age
+    #Letting them know they're ineligible and the amount of years they still have left to be able to vote
+    print(f"Oops! You're not eligible yet. But hey, only {years_left} more year{'s' if years_left > 1 else ''} to go!")
+
